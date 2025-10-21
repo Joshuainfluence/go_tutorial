17e1df5fe3e1b5e0d84101f5bfac58f20523cf8f
joshuainfluence <joshuajulius2030@gmail.com>
2025-10-21 22:54:42 +0100

    basic addition

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ func main()  {
 	// create a varible
 	const name = "Joshua"
 
+	const secondsInMinute = 60
+	const minutesInHour = 60 
+	const secondsInHour = secondsInMinute * minutesInHour
+
+
 	// print result
 	fmt.Println("Name: ", name)
+	fmt.Println("Number of seconds in an hour: ", secondsInHour)
+
 }
